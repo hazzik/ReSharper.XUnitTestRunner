e1cadc118fa3575c934148f54317fe3e7241d523
citizenmatt <m.t.ellis@gmail.com>
2009-12-07 22:08:12 +0000

    Refactored provider assembly to be a bit more sensibly organised, and to use JetBrain's LINQ implementation (so I can still use C#3, but target .net 2 - yay!) Migrated refactoring across all 3 versions - 4.1, 4.5 and 5.0. Sheesh.
    --HG--
    extra : convert_revision : svn%3Afeebac22-3a02-2946-87e6-b637fb197462%4044

---
+++ resharper/xunitcontrib.runner.resharper.provider.4.1/IMetadataTypeProcessor.cs
@@ -0,0 +1,9 @@
+﻿using JetBrains.Metadata.Reader.API;
+
+namespace XunitContrib.Runner.ReSharper.UnitTestProvider
+{
+    internal interface IMetadataTypeProcessor
+    {
+        void ProcessTypeInfo(IMetadataTypeInfo metadataTypeInfo);
+    }
+}
