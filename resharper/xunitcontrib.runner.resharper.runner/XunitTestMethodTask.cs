41469350605c90c00b01c4abd345e2fa56bb1484
hazzik <hazzik@gmail.com>
2010-12-24 22:50:30 +0500

    removed obsolete interface IUnitTestRemoteTask
    --HG--
    branch : R#6
    rename : resharper/xunitcontrib.runner.resharper.runner.5.1/XunitTestMethodTask.cs => resharper/xunitcontrib.runner.resharper.runner/XunitTestMethodTask.cs
    extra : rebase_source : 2d59ceba7afe015bc33ee0ea425abeb39d15b2fd

--- resharper/xunitcontrib.runner.resharper.runner/XunitTestMethodTask.cs
+++ resharper/xunitcontrib.runner.resharper.runner/XunitTestMethodTask.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Xml;
+using JetBrains.Annotations;
 using JetBrains.ReSharper.TaskRunnerFramework;
 
 namespace XunitContrib.Runner.ReSharper.RemoteRunner
 {
     [Serializable]
-    public class XunitTestMethodTask : RemoteTask, IUnitTestRemoteTask, IEquatable<XunitTestMethodTask>
+    public class XunitTestMethodTask : RemoteTask, IEquatable<XunitTestMethodTask>
     {
         readonly bool explicitly;
         readonly string methodName;
@@ -38,6 +39,7 @@ namespace XunitContrib.Runner.ReSharper.RemoteRunner
         // That framework retrieves these tasks from devenv/resharper via remoting (hence
         // the SerializableAttribute) but uses this hand rolled xml serialisation to
         // get the tasks into the app domain that will actually run the tests
+        [UsedImplicitly]
         public XunitTestMethodTask(XmlElement element) : base(element)
         {
             assemblyLocation = GetXmlAttribute(element, AttributeNames.AssemblyLocation);
