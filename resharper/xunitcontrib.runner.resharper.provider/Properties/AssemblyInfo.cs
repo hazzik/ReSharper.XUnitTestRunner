104e573b712283cc40446f2db2f95cf7e95a5ff4
xelibrion <xelibrion@gmail.com>
2011-03-31 11:25:54 +0400

    CommonAssemblyInfo.cs enabled
    --HG--
    branch : R#6

--- resharper/xunitcontrib.runner.resharper.provider/Properties/AssemblyInfo.cs
+++ resharper/xunitcontrib.runner.resharper.provider/Properties/AssemblyInfo.cs
@@ -7,11 +7,3 @@ using JetBrains.Application.PluginSupport;
 [assembly : PluginDescription("Allows ReSharper 5.1 to run unit tests from xUnit.net")]
 [assembly : PluginVendor("xunitcontrib Team")]
 
-[assembly: AssemblyCompany("http://xunitcontrib.codeplex.com")]
-[assembly: AssemblyProduct("xunitcontrib.runner.resharper")]
-[assembly: AssemblyDescription("xUnit.net unit test provider for Resharper")]
-[assembly: AssemblyCopyright("Copyright (C) xunitcontrib team")]
-[assembly: ComVisible(false)]
-
-// This build number will track the ReSharper 5.1 nightly build number
-[assembly: AssemblyVersion("0.4.1.1709")]
