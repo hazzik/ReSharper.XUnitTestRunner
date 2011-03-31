0683711dc17603971d5a8133a8b5a8df15a9079c
xelibrion <xelibrion@gmail.com>
2011-03-31 11:27:51 +0400

    Descriptions have been fixed
    --HG--
    branch : R#6

--- resharper/xunitcontrib.runner.resharper.provider/Properties/AssemblyInfo.cs
+++ resharper/xunitcontrib.runner.resharper.provider/Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 using JetBrains.Application.PluginSupport;
 
-[assembly : AssemblyTitle("xUnit.net unit test provider for ReSharper 5.1")]
-[assembly : PluginTitle("xUnit.net unit test provider for ReSharper 5.1")]
-[assembly : PluginDescription("Allows ReSharper 5.1 to run unit tests from xUnit.net")]
-[assembly : PluginVendor("xunitcontrib Team")]
-
+[assembly: AssemblyTitle("xUnit.net unit test provider for ReSharper 6.0 EAP")]
+[assembly: PluginTitle("xUnit.net unit test provider for ReSharper 6.0 EAP")]
+[assembly: PluginDescription("Allows ReSharper 6.0 EAP to run unit tests from xUnit.net")]
+[assembly: PluginVendor("xunitcontrib Team")]
--- resharper/xunitcontrib.runner.resharper.runner/Properties/AssemblyInfo.cs
+++ resharper/xunitcontrib.runner.resharper.runner/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("xUnit.net remote unit test runner for ReSharper 5.1")]
+[assembly: AssemblyTitle("xUnit.net remote unit test runner for ReSharper 6.0 EAP")]
