a0e5ca354c0222bcad840f96828544cbb639be41
hazzik <hazzik@gmail.com>
2011-11-23 18:02:36 +0600

    Added more tests

--- src/XUnitTestProvider.Tests/UnitTestElementPsiIdentifierTests.cs
+++ src/XUnitTestProvider.Tests/UnitTestElementPsiIdentifierTests.cs
@@ -8,60 +8,324 @@ namespace XUnitTestProvider.Tests
 
     public class UnitTestElementPsiIdentifierTests : PsiFileBaseTests
     {
+        private void DoTest(Action<IDeclaredElement> action, params string[] sources)
+        {
+            WithEachPsiFile(file => file.ProcessDescendants(new RecursiveElementProcessor(action)), sources);
+        }
+
+        private class RecursiveElementProcessor : IRecursiveElementProcessor
+        {
+            private readonly Action<IDeclaredElement> action;
+
+            public RecursiveElementProcessor(Action<IDeclaredElement> action)
+            {
+                this.action = action;
+            }
+
+            #region IRecursiveElementProcessor Members
+
+            public bool InteriorShouldBeProcessed(ITreeNode element)
+            {
+                return !(element is ITypeMemberDeclaration) || element is ITypeDeclaration;
+            }
+
+            public void ProcessBeforeInterior(ITreeNode element)
+            {
+                var declaration = element as IDeclaration;
+                if (declaration != null)
+                    action(declaration.DeclaredElement);
+            }
+
+            public void ProcessAfterInterior(ITreeNode element)
+            {
+            }
+
+            public bool ProcessingIsFinished
+            {
+                get { return false; }
+            }
+
+            #endregion
+        }
+
         [Test]
         public void ShouldMarkPublicClassAsPublic()
         {
             bool isPublic = false;
-            WithEachPsiFile(file =>
-                            file.ProcessDescendants(
-                                new RecursiveElementProcessor(
-                                    node =>
-                                        {
-                                            var @class = node as IClass;
-                                            if (@class != null)
-                                            {
-                                                if (@class.ShortName == "PublicClass")
-                                                {
-                                                    isPublic = UnitTestElementPsiIdentifier.IsPublic(@class);
-                                                }
-                                            }
-                                        })), "PublicClass.cs");
+            Action<IDeclaredElement> action =
+                node =>
+                    {
+                        var @class = node as IClass;
+                        if (@class != null)
+                        {
+                            if (@class.ShortName == "PublicClass")
+                            {
+                                isPublic = UnitTestElementPsiIdentifier.IsPublic(@class);
+                            }
+                        }
+                    };
+            DoTest(action, "IsPublicTests.cs");
             Assert.IsTrue(isPublic);
         }
-    }
-
-    public class RecursiveElementProcessor : IRecursiveElementProcessor
-    {
-        private readonly Action<IDeclaredElement> action;
-
-        public RecursiveElementProcessor(Action<IDeclaredElement> action)
+        
+        [Test]
+        public void ShouldMarkPublicMethodInPublicClassAsPublic()
         {
-            this.action = action;
+            bool isPublic = false;
+            Action<IDeclaredElement> action =
+                node =>
+                    {
+                        var method = node as IMethod;
+                        if (method != null)
+                        {
+                            if (method.ShortName == "PublicMethod")
+                            {
+                                isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                            }
+                        }
+                    };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.IsTrue(isPublic);
+        }
+        
+        [Test]
+        public void ShouldMarkPublicStaticMethodInPublicClassAsPublic()
+        {
+            bool isPublic = false;
+            Action<IDeclaredElement> action =
+                node =>
+                    {
+                        var method = node as IMethod;
+                        if (method != null)
+                        {
+                            if (method.ShortName == "PublicStaticMethod")
+                            {
+                                isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                            }
+                        }
+                    };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.IsTrue(isPublic);
+        }
+        
+        [Test]
+        public void ShouldNotMarkProtectedMethodInPublicClassAsPublic()
+        {
+            bool isPublic = true;
+            Action<IDeclaredElement> action =
+                node =>
+                    {
+                        var method = node as IMethod;
+                        if (method != null)
+                        {
+                            if (method.ShortName == "ProtectedMethod")
+                            {
+                                isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                            }
+                        }
+                    };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.False(isPublic);
+        }
+        
+        [Test]
+        public void ShouldNotMarkProtectedStaticMethodInPublicClassAsPublic()
+        {
+            bool isPublic = true;
+            Action<IDeclaredElement> action =
+                node =>
+                    {
+                        var method = node as IMethod;
+                        if (method != null)
+                        {
+                            if (method.ShortName == "ProtectedStaticMethod")
+                            {
+                                isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                            }
+                        }
+                    };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.False(isPublic);
+        }
+        
+        [Test]
+        public void ShouldNotMarkPrivateMethodInPublicClassAsPublic()
+        {
+            bool isPublic = true;
+            Action<IDeclaredElement> action =
+                node =>
+                    {
+                        var method = node as IMethod;
+                        if (method != null)
+                        {
+                            if (method.ShortName == "PrivateMethod")
+                            {
+                                isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                            }
+                        }
+                    };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.False(isPublic);
         }
 
-        #region IRecursiveElementProcessor Members
-
-        public bool InteriorShouldBeProcessed(ITreeNode element)
+        [Test]
+        public void ShouldNotMarkPrivateStaticMethodInPublicClassAsPublic()
         {
-            return !(element is ITypeMemberDeclaration) || element is ITypeDeclaration;
+            bool isPublic = true;
+            Action<IDeclaredElement> action =
+                node =>
+                    {
+                        var method = node as IMethod;
+                        if (method != null)
+                        {
+                            if (method.ShortName == "PrivateStaticMethod")
+                            {
+                                isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                            }
+                        }
+                    };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.False(isPublic);
+        }
+        
+        [Test]
+        public void ShouldMarkNestedPublicClassAsPublic()
+        {
+            bool isPublic = false;
+            Action<IDeclaredElement> action =
+                node =>
+                    {
+                        var @class = node as IClass;
+                        if (@class != null)
+                        {
+                            if (@class.ShortName == "NestedPublicClass")
+                            {
+                                isPublic = UnitTestElementPsiIdentifier.IsPublic(@class);
+                            }
+                        }
+                    };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.IsTrue(isPublic);
         }
 
-        public void ProcessBeforeInterior(ITreeNode element)
+        [Test]
+        public void ShouldMarkPublicMethodInNestedPublicClassAsPublic()
         {
-            var declaration = element as IDeclaration;
-            if (declaration != null)
-                action(declaration.DeclaredElement);
+            bool isPublic = false;
+            Action<IDeclaredElement> action =
+                node =>
+                {
+                    var method = node as IMethod;
+                    if (method != null)
+                    {
+                        if (method.ShortName == "NestedPublicMethod")
+                        {
+                            isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                        }
+                    }
+                };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.IsTrue(isPublic);
         }
 
-        public void ProcessAfterInterior(ITreeNode element)
+        [Test]
+        public void ShouldMarkPublicStaticMethodInNestedPublicClassAsPublic()
         {
+            bool isPublic = false;
+            Action<IDeclaredElement> action =
+                node =>
+                {
+                    var method = node as IMethod;
+                    if (method != null)
+                    {
+                        if (method.ShortName == "NestedPublicStaticMethod")
+                        {
+                            isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                        }
+                    }
+                };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.IsTrue(isPublic);
         }
 
-        public bool ProcessingIsFinished
+        [Test]
+        public void ShouldNotMarkProtectedMethodInNestedPublicClassAsPublic()
         {
-            get { return false; }
+            bool isPublic = true;
+            Action<IDeclaredElement> action =
+                node =>
+                {
+                    var method = node as IMethod;
+                    if (method != null)
+                    {
+                        if (method.ShortName == "NestedProtectedMethod")
+                        {
+                            isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                        }
+                    }
+                };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.False(isPublic);
         }
 
-        #endregion
+        [Test]
+        public void ShouldNotMarkProtectedStaticMethodInNestedPublicClassAsPublic()
+        {
+            bool isPublic = true;
+            Action<IDeclaredElement> action =
+                node =>
+                {
+                    var method = node as IMethod;
+                    if (method != null)
+                    {
+                        if (method.ShortName == "NestedProtectedStaticMethod")
+                        {
+                            isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                        }
+                    }
+                };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.False(isPublic);
+        }
+
+        [Test]
+        public void ShouldNotMarkPrivateMethodInNestedPublicClassAsPublic()
+        {
+            bool isPublic = true;
+            Action<IDeclaredElement> action =
+                node =>
+                {
+                    var method = node as IMethod;
+                    if (method != null)
+                    {
+                        if (method.ShortName == "NestedPrivateMethod")
+                        {
+                            isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                        }
+                    }
+                };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.False(isPublic);
+        }
+
+        [Test]
+        public void ShouldNotMarkPrivateStaticMethodInNestedPublicClassAsPublic()
+        {
+            bool isPublic = true;
+            Action<IDeclaredElement> action =
+                node =>
+                {
+                    var method = node as IMethod;
+                    if (method != null)
+                    {
+                        if (method.ShortName == "NestedPrivateStaticMethod")
+                        {
+                            isPublic = UnitTestElementPsiIdentifier.IsPublic(method);
+                        }
+                    }
+                };
+            DoTest(action, "IsPublicTests.cs");
+            Assert.False(isPublic);
+        }
     }
 }
---
+++ test/data/UnitTestElementPsiIdentifierTests/IsPublicTests.cs
@@ -0,0 +1,53 @@
+public class PublicClass 
+{
+    public void PublicMethod() 
+    {
+    }
+    
+    public static void PublicStaticMethod() 
+    {
+    }
+    
+    protected void ProtectedMethod() 
+    {
+    }
+
+    protected static void ProtectedStaticMethod() 
+    {
+    }
+
+    private void PrivateMethod() 
+    {
+    }
+
+    private static void PrivateStaticMethod() 
+    {
+    }
+
+    public class NestedPublicClass 
+    {
+        public void NestedPublicMethod() 
+        {
+        }
+    
+        public static void NestedPublicStaticMethod() 
+        {
+        }
+    
+        protected void NestedProtectedMethod() 
+        {
+        }
+
+        protected static void NestedProtectedStaticMethod() 
+        {
+        }
+
+        private void NestedPrivateMethod() 
+        {
+        }
+
+        private static void NestedPrivateStaticMethod() 
+        {
+        }
+    }
+}
