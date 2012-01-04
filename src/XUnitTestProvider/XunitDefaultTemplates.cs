9175984dae9712dda9647abecc664139311ed71f
hazzik <hazzik@gmail.com>
2012-01-05 00:20:29 +0600

    Added live templates installer

---
+++ src/XUnitTestProvider/XunitDefaultTemplates.cs
@@ -0,0 +1,24 @@
+﻿namespace ReSharper.XUnitTestProvider
+{
+    using System.IO;
+    using JetBrains.Application;
+    using JetBrains.Application.Settings;
+    using JetBrains.DataFlow;
+    using Properties;
+
+    [ShellComponent]
+    public class XunitDefaultTemplates : IHaveDefaultSettingsStream
+    {
+        public Stream GetDefaultSettingsStream(Lifetime lifetime)
+        {
+            var stream = new MemoryStream(Resources.LiveTemplates);
+            lifetime.AddDispose(stream);
+            return stream;
+        }
+
+        public string Name
+        {
+            get { return "xUnit.net Default Templates"; }
+        }
+    }
+}
