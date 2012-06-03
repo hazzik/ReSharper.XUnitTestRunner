a3124211693d8dd601b790f4fb78f9c701ebbc73
hazzik <hazzik@gmail.com>
2012-06-03 17:20:52 +0600

    TemplatesInstaller - install templates for all VS versions

--- src/Setup.TemplatesInstaller/Program.cs
+++ src/Setup.TemplatesInstaller/Program.cs
@@ -3,8 +3,8 @@ using System.Linq;
 using System.Xml.Linq;
 
 namespace Setup.TemplatesInstaller
-{    
-    class Program
+{
+    static class Program
     {
         static int Main(string[] args)
         {
@@ -13,30 +13,14 @@ namespace Setup.TemplatesInstaller
 
             try
             {
-                var userSettingsPath = Path.Combine(args[0], "UserSettings.xml");
-                var liveTemplatesPath = Path.Combine(args[0], args[1]);
+                var installed =
+                    InstallTemplates(args[0], "", args[1]) | //All versions
+                    InstallTemplates(args[0], "vs10.0", args[1]) | // vs2010
+                    InstallTemplates(args[0], "vs9.0", args[1]) |  // vs2008
+                    InstallTemplates(args[0], "vs8.0", args[1]);  // vs2005
 
-                var userSettings = XDocument.Parse(File.ReadAllText(userSettingsPath));
-                var liveTemplates = XDocument.Load(File.OpenRead(liveTemplatesPath));
-
-                var userTemplates = userSettings.Descendants("LiveTemplatesManager")
-                    .Elements("UserTemplates")
-                    .Single();
-
-                foreach (var liveTemplate in liveTemplates.Descendants("Template"))
-                {
-                    var installedTemplate = userTemplates.Elements("Template")
-                        .SingleOrDefault(x => x.Attribute("uid").Value == liveTemplate.Attribute("uid").Value);
-
-                    liveTemplate.SetAttributeValue("xunit-resharper", "true");
-
-                    if (installedTemplate != null)
-                        installedTemplate.ReplaceWith(liveTemplate);
-                    else
-                        userTemplates.Add(liveTemplate);
-                }
-
-                userSettings.Save(userSettingsPath);
+                if (!installed)
+                    return 1;
             }
             catch
             {
@@ -44,6 +28,39 @@ namespace Setup.TemplatesInstaller
             }
 
             return 0;
+        }
+
+        private static bool InstallTemplates(string path, string vsVersion, string templates)
+        {
+            var userSettingsPath = Path.Combine(Path.Combine(path, vsVersion), "UserSettings.xml");
+            var liveTemplatesPath = Path.Combine(path, templates);
+
+            if (!File.Exists(userSettingsPath))
+                return false;
+
+            var userSettings = XDocument.Parse(File.ReadAllText(userSettingsPath));
+            var liveTemplates = XDocument.Load(new StreamReader(File.OpenRead(liveTemplatesPath)));
+
+            var userTemplates = userSettings.Descendants("LiveTemplatesManager")
+                .Elements("UserTemplates")
+                .Single();
+
+            foreach (var liveTemplate in liveTemplates.Descendants("Template"))
+            {
+                var installedTemplate = userTemplates.Elements("Template")
+                    .SingleOrDefault(x => x.Attribute("uid").Value == liveTemplate.Attribute("uid").Value);
+
+                liveTemplate.SetAttributeValue("xunit-resharper", "true");
+
+                if (installedTemplate != null)
+                    installedTemplate.ReplaceWith(liveTemplate);
+                else
+                    userTemplates.Add(liveTemplate);
+            }
+
+            userSettings.Save(userSettingsPath);
+
+            return true;
         }
     }
 }
