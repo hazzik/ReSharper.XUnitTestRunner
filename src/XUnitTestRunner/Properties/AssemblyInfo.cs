54a7815d67b6a9c7519f1b77b206088d04694371
Alexander I. Zaytsev <hazzik@gmail.com>
2012-09-30 00:08:08 +0600

    Change version to 7.0RTM

--- src/XUnitTestRunner/Properties/AssemblyInfo.cs
+++ src/XUnitTestRunner/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("xUnit.net remote unit test runner for ReSharper 7.0 EAP")]
+[assembly: AssemblyTitle("xUnit.net remote unit test runner for ReSharper 7.0 RTM")]
